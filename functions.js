0e9476aad4cd1888a32de205234cafef564305f3
Patrick Dantas <patrick.dantas@inventsoftware.com.br>
2018-10-01 15:59:42 -0300

    .

--- functions.js
+++ functions.js
@@ -10,33 +10,35 @@ if(gl){
     if(gl){
         cab.innerHTML = "Suporta WebGL";
     }
-}    
+}
 
 gl.clearColor(0.3, 0.3 ,0.3 ,1);
-gl.clear(gl.COLOR_BUFFER_BIT);            
+gl.clear(gl.COLOR_BUFFER_BIT);
 
-let vertex = gl.createShader(gl.VERTEX_SHADER);
-
-gl.shaderSource(vertex,[
+let vertex_shader = [
     "attribute vec3 pos;",
     "void main()",
     "{",
     "	gl_Position = vec4(pos, 1.);",
     "	gl_PointSize = 10.;",
     "}"	
-].join('\n'));
+].join('\n');
 
+let vertex = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertex, vertex_shader);
 gl.compileShader(vertex);
-let fragment = gl.createShader(gl.FRAGMENT_SHADER);
 
-gl.shaderSource(fragment,[                
+let fragment_shader = [
     "void main()",
     "{",
     "	gl_FragColor = vec4(1.,1.,1.,1.);",
     "}"	
-].join('\n'));
+].join('\n');
 
+let fragment = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragment, fragment_shader);
 gl.compileShader(fragment);
+
 let program = gl.createProgram();
 gl.attachShader(program, vertex);
 gl.attachShader(program, fragment);
@@ -44,11 +46,11 @@ gl.attachShader(program, fragment);
 gl.linkProgram(program);
 gl.useProgram(program);
 
-let point = [0.5, 0.5, 0.5, 0, 0, 0];
+let point = [0.5,0.5,0.5, 0,0,0, 0.7,0.3,0.3, -0.4,-0.4,-0.4];
 let vbo = gl.createBuffer();
 
 gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point) ,gl.STATIC_DRAW);
 gl.enableVertexAttribArray(0);
 gl.vertexAttribPointer(0,3,gl.FLOAT, false, 0,0);
-gl.drawArrays(gl.POINTS, 0, 2);
+gl.drawArrays(gl.TRIANGLES, 0, 4);
